f465527d31206507c9e6f58ac010cf260058e72c
Rinat <angrykeo@gmail.com>
2022-02-08 18:28:46 +0300

    Добавил слэш в urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -22,5 +22,5 @@ from products.views import products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products', products, name='products')
+    path('products/', products, name='products')
 ]
